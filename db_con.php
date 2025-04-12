bf64216ddc63fa91128c246d4c0df77ee7b3f2dd
Mohammadmehdi-web <webauctech110@gmail.com>
2025-04-12 12:54:55 +0530

    add admin folders

--- db_con.php
+++ db_con.php
@@ -7,6 +7,6 @@
     $con = new mysqli($servername, $username, $password, $database);
 
     if($con->connect_error){
-        die("Connection Failed: " . $con->connect_error);
+        die("Connection Failed: " . $con->connect_error);ghggghghjs
     }
 ?>
